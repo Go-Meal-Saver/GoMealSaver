a0197fc3f113fd8d305b41692f6de27700bc712f
Anezz12 <harsenaargrtya1@gmail.com>
2024-12-02 17:26:04 +0700

    Refactor MessageCard component to improve reply handling, enhance message display, and update convertToSerializedObject function for null checks

--- utils/convertToObject.js
+++ utils/convertToObject.js
@@ -1,13 +1,13 @@
 export function convertToSerializedObject(leanDocument) {
-  if (!leanDocument) {
+  if (leanDocument == null) {
     return leanDocument;
   }
 
   for (const key of Object.keys(leanDocument)) {
     if (
-      leanDocument[key] &&
-      typeof leanDocument[key].toJSON === 'function' &&
-      typeof leanDocument[key].toString === 'function'
+      leanDocument[key] != null &&
+      typeof leanDocument[key]?.toJSON === 'function' &&
+      typeof leanDocument[key]?.toString === 'function'
     ) {
       leanDocument[key] = leanDocument[key].toString();
     }
